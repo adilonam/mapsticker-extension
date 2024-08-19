927b34e063eeaf2f8a5041ad226503d3f6427b1f
Adil ABBADI <adil.abbadi.1996@gmail.com>
2024-08-19 16:45:07 +0100

    large popup

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,13 @@ import './App.css'
 function App() {
 
   const appUrl = import.meta.env.VITE_APP_URL;
-  const size = 100
 
   const openApp = () => {
     
     window.open(
       appUrl,
       'AppPopup',
-      'width=700,height=600,left=100,top=100,menubar=no,toolbar=no,status=no,scrollbars=no,resizable=no'
+      'width=1000,height=700,left=100,top=100,menubar=no,toolbar=no,status=no,scrollbars=no,resizable=no'
     );
   };
 
@@ -21,48 +20,7 @@ function App() {
   return (
     <>
      <h1>Map Sticker</h1>
-     <div>
-
-     <img
-      src={"/stickers/1.png"}
-      draggable="true"
-      style={{ cursor: 'grab' }}
-      width={size}
-    />
-      <img
-      src={"/stickers/2.png"}
-      draggable="true"
-      style={{ cursor: 'grab' }}
-      width={size}
-    />
-    <img
-      src={"/stickers/3.png"}
-      draggable="true"
-      style={{ cursor: 'grab' }}
-      width={size}
-    />
-     <img
-      src={"/stickers/4.png"}
-      draggable="true"
-      style={{ cursor: 'grab' }}
-      width={size}
-    />
-      <img
-      src={"/stickers/5.png"}
-      draggable="true"
-      style={{ cursor: 'grab' }}
-      width={size}
-    />
-    <img
-      src={"/stickers/6.png"}
-      draggable="true"
-      style={{ cursor: 'grab' }}
-      width={size}
-    />
-     </div>
      
-    
-    
     <button
       onClick={openApp}
       className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
